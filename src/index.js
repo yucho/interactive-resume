4d1c08117227f1534d384079979602d9bb85c669
yucho <yucho.ho@gmail.com>
2019-05-16 10:33:40 -0700

    Dev server with hot reload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // Test webpack here
-import * as styles from './test.css';
+import * as styles from './test.module.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const div = document.createElement('div');
   div.innerText = 'Testing webpack!';
   div.classList.add(styles.composed);
   document.body.appendChild(div);
+  console.log(styles);
 });
--- src/server.js
+++ src/server.js
@@ -1,17 +1,43 @@
-const express = require('express');
+/**
+ * Copy files from src to dest
+ */
 const path = require('path');
 const fs = require('fs');
-
-const src = (file) => path.resolve(__dirname, file);
-const dest = (...file) => path.resolve(__dirname, '..', 'dist', ...file);
-fs.copyFileSync(src('index.html'), dest('index.html'), (err) => {
+const paths = (...file) => path.resolve(__dirname, ...file);
+fs.copyFileSync(paths('index.html'), paths('..', 'dist', 'index.html'), (err) => {
   if (err) throw err;
 
   console.log('index.html copied to dist');
 });
 
-const app = express();
-app.use(express.static(dest()));
 
-const port = process.env.PORT || 8080;
+/**
+ * Hot reload (no auto refresh)
+ */
+const watcher = require('chokidar').watch(paths('..', 'dist'));
+watcher.on('ready', () => {
+  watcher.on('all', () => {
+    console.log('File change detected! Clearing module cache from server');
+    for(const id in require.cache) {
+      if (/[\/\\]dist[\/\\]/.test(id)) delete require.cache[id];
+    }
+  });
+});
+
+
+/**
+ * Run a server that serves static files
+ */
+const express = require('express');
+const app = express();
+app.use(express.static(paths('..', 'dist')));
+
+const port = 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+/**
+ * Opens your browser automatically
+ */
+const open = require('open');
+open(`http://localhost:${port}`);
