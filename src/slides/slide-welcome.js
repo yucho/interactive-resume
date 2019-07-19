9575429c9dd1fc1ef9c9c1df1198d183dbb93c47
yucho <yucho.ho@gmail.com>
2019-07-19 12:09:47 -0700

    Display welcome

--- src/slides/slide-welcome.js
+++ src/slides/slide-welcome.js
@@ -1,13 +1,34 @@
 import Slide from './slide.js';
+import * as style from '../css/slide-welcome.module.css';
+import { addClasses } from '../util.js';
+import DOMElements from './dom-elements.js';
 
 class Welcome extends Slide {
   constructor(data) {
     super(data);
+    this.createDomElements();
     this.on('enter', this.onEnter);
+    this.on('exit', this.onExit);
+  }
+
+  createDomElements() {
+    const welcome = document.createElement('h1');
+    welcome.innerHTML = 'WELCOME';
+    addClasses(welcome, style.welcome);
+
+    this.domElements.elements.push(welcome);
   }
 
   onEnter() {
-    console.log('entered');
+    for (const el of this.domElements.elements) {
+      DOMElements.container.appendChild(el);
+    }
+  }
+
+  onExit() {
+    for (const el of this.domElements.elements) {
+      DOMElements.container.removeChild(el);
+    }
   }
 }
 
