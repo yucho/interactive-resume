7c8b6511fc26a41ca12879ad123f005a4464a66f
yucho <yucho.ho@gmail.com>
2019-07-19 13:04:31 -0700

    Bugfix scroll overshoot

--- src/controllers/controller-main.js
+++ src/controllers/controller-main.js
@@ -1,9 +1,13 @@
 export default class ControllerMain {
   constructor(interactive) {
     this.container = interactive.container;
-    this.container.addEventListener('mousewheel', (e) => {
-      if (Math.abs(e.deltaY) > 20) {
+    this.container.addEventListener('wheel', (e) => {
+      if (Math.abs(e.deltaY) < 20) {
+        this.flag = true;
+      }
+      else if (this.flag) {
         e.deltaY > 0 ? interactive.next() : interactive.prev();
+        this.flag = false;
       }
     });
   }
