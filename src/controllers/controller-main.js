64bd22b5e0904b4e72859b624949274994f65e7d
yucho <yucho.ho@gmail.com>
2019-07-24 00:38:54 -0700

    Bugfix mousewheel control and make it less clunky

--- src/controllers/controller-main.js
+++ src/controllers/controller-main.js
@@ -1,14 +1,33 @@
 export default class ControllerMain {
   constructor(interactive) {
     this.container = interactive.container;
-    this.container.addEventListener('wheel', (e) => {
-      if (Math.abs(e.deltaY) < 20) {
-        this.flag = true;
+    this.interactive = interactive;
+    this.addScrollControl();
+  }
+
+  addScrollControl() {
+    let flag, timeout, supportsWheel, prevDeltaY = 0;
+    const clearFlag = () => {
+      const waitTime = 200;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => flag = false, waitTime);
+    };
+    const onScroll = (e) => {
+      if (e.type === 'wheel') {
+        supportsWheel = true;
+      } else if (supportsWheel) {
+        return;
       }
-      else if (this.flag) {
-        e.deltaY > 0 ? interactive.next() : interactive.prev();
-        this.flag = false;
+
+      if (!flag && Math.abs(e.deltaY) > 5 && Math.abs(e.deltaY) > Math.abs(prevDeltaY)) {
+        e.deltaY > 0 ? this.interactive.next() : this.interactive.prev();
+        flag = true;
+        clearFlag();
       }
-    });
+      prevDeltaY = e.deltaY;
+    }
+    this.container.addEventListener('wheel', onScroll);
+    this.container.addEventListener('mousewheel', onScroll);
+    this.container.addEventListener('DOMMouseScroll', onScroll);
   }
 };
